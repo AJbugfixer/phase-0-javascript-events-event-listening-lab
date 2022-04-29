6726a729e8e3620209702f178bcae447a0360782
Ajay <ajay.ip3105@gmail.com>
2022-04-28 22:54:39 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const input =document.getElementById('input')
+    function clickAlert(){
+        alert('I was Clicked!')
+    }
+    input.addEventListener('click', clickAlert)
 }
+addingEventListener()
